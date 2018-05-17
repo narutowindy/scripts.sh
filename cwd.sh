88b755f5a425013fce0393cb66a62da361cb5fbc
Naruto Windy <narutowindy@users.noreply.github.com>
2018-05-16 21:30:18 -0400

    Add files via upload

---
+++ cwd.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+hostname=`hostname`
+export PWD=`i3-msg -t get_tree | sed 's/{/{\n/g' | grep -A 5 "\"focused\":true" | sed 's/,/\n/g' | grep $hostname | sed 's/\"//g' | awk 'BEGIN {FS=":"};{print $3}' | sed 's/\~/\/root/g' | head -1` 
+
+cd "$PWD"
+exec urxvt & 
+disown 
+
+#exec urxvt -e /bin/sh -c 'cd "$PWD" && /bin/bash'
